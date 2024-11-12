190ee9ee4c73c8c8fe6a0d54e0535b03ac464139
aniketvw <aniketvw14@gmail.com>
2024-11-12 08:26:00 +0530

    feat : Adding Mongo/SQL identifiers

--- microservices/product-service/src/main/java/se/aw/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/aw/microservices/core/product/ProductServiceApplication.java
@@ -1,16 +1,61 @@
 package se.aw.microservices.core.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.IndexResolver;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
+import se.aw.microservices.core.product.persistence.ProductEntity;
 
 @SpringBootApplication
 @ComponentScan("se.aw")
 
 public class ProductServiceApplication {
 
+	private static final Logger LOG= LoggerFactory.getLogger(ProductServiceApplication.class);
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+
+		String mongoDbHost= ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+		String mongodDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+
+		LOG.info("Connected to MongoDb: " + mongoDbHost + ":" + mongodDbPort);
+
+
 	}
 
+	@Autowired
+	MongoOperations mongoTemplate;
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void initIndicesAfterStartup(){
+		//Retrieve metadata about MongoDB entities,metadata is used to resolve what indexes are there for productEntity
+		MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext=
+				mongoTemplate.getConverter().getMappingContext();
+
+		//Get the required Indexes, fields annotated with @Indexed
+		IndexResolver resolver= new MongoPersistentEntityIndexResolver(mappingContext);
+
+		//Get access to Indexed operations
+		IndexOperations indexOps= mongoTemplate.indexOps(ProductEntity.class);
+
+		//Verify if indexes are created...Resolver took from entity, indexOps takes index from mongoDb.
+		//If index is not created it will be created
+		resolver.resolveIndexFor(ProductEntity.class).forEach(indexOps::ensureIndex);
+
+	}
 }
--- microservices/review-service/src/main/java/se/aw/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/se/aw/microservices/core/review/ReviewServiceApplication.java
@@ -1,16 +1,27 @@
 package se.aw.microservices.core.review;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("se.aw")
 public class ReviewServiceApplication {
 
+	private static final Logger LOG= LoggerFactory.getLogger(ReviewServiceApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewServiceApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(ReviewServiceApplication.class, args);
+
+		String mySqlUri= ctx.getEnvironment().getProperty("");
+
+		LOG.info("Connected to MySQL:" +mySqlUri);
+
 	}
 
 }
